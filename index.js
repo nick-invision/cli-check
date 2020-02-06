ed9845e98ae6da6e95c6a337baf4c2424813c038
Nick Fields <50085412+nick-invision@users.noreply.github.com>
2020-02-06 18:22:58 -0500

    Added 'contains' comparison (#1)
    * patch: initial commit for compare command
    
    * patch: renamed to cli-if
    
    * patch: removed unused devDeps and added files list
    
    * docs: added readme

---
+++ index.js
@@ -0,0 +1,35 @@
+const { execSync } = require('child_process');
+
+const args = process.argv;
+const supported = [
+    'contains'
+]
+const root = args[2]
+const expected = args[3];
+const actual = args[4];
+const cmd = args.slice(5).join(' ');
+
+if (args.length < 5) {
+    throw new Error(`Required format: <${supported.join('|')}> <expected> <actual> [cmd to execute if true]`)
+} else if (!supported.some(cmd => cmd === root)) {
+    throw new Error (`Command '${root}' is not supported.  Must be one of ${supported.join('|')}`)
+}
+
+const contains = (expected, actual) => {
+    return `${actual}`.includes(expected)
+}
+
+(()=>{
+    switch (root) {
+        case 'contains':
+            if (!contains(expected, actual)) {
+                console.error(`${actual} does not contain ${expected}.  Exiting.`)
+                process.exit(0);
+            }
+            break;
+        default:
+            throw new Error(`Command '${root}' is not yet implemented.`)
+    }
+
+    execSync(cmd, {stdio: 'inherit'})
+})()
